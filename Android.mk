512f54d4023dcf3412d3007c91140dd7dbf0e067
Ratchanan Srirattanamet <peathot@hotmail.com>
2015-07-02 02:15:02 +0700

    Comment out Java component in Android.mk
    Ubuntu touch hates Java, so this make build system happy.

--- Android.mk
+++ Android.mk
@@ -7,14 +7,14 @@ LOCAL_SHARED_LIBRARIES := libcutils
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
 
-include $(CLEAR_VARS)
+#include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_PACKAGE_NAME := TimeKeep
-LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := true
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_SRC_FILES := $(call all-java-files-under, src)
+#LOCAL_PACKAGE_NAME := TimeKeep
+#LOCAL_CERTIFICATE := platform
+#LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_PROGUARD_ENABLED := disabled
+#LOCAL_PROGUARD_ENABLED := disabled
 
-include $(BUILD_PACKAGE)
+#include $(BUILD_PACKAGE)
